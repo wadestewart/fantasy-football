de1012175e632c35787269d3ce8634699c6b1f3c
Wade Stewart <stew.wade@gmail.com>
2018-02-20 14:41:30 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
+  # get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :teams do
+    resources :players
+  end
+  
 end
