5d5e051afb040d9ec17cd7c006ee50c34a596161
Wade Stewart <stew.wade@gmail.com>
2018-02-20 17:55:07 -0500

    Added Team seed data

--- README.md
+++ README.md
@@ -1,25 +1,37 @@
-# README
+# Fantasy Football
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
-# fantasy-football
+Key: 9rbsqdx5jmv7rmj3bqz35wwz
+Teams:
+TB: 4254d319-1bc7-4f81-b4ab-b5e6f3402b69
+BAL: ebd87119-b331-4469-9ea6-d51fe3ce2f1c
+LAC: 1f6dcffb-9823-43cd-9ff4-e7a8466749b5
+LAR: 2eff2a03-54d4-46ba-890e-2bc3925548f3
+PIT: cb2f9f1f-ac67-424e-9e72-1475cb0ed398
+MIN: 33405046-04ee-4058-a950-d606f8c30852
+NYG: 04aa1c9d-66da-489d-b16a-1dee3f2eec4d
+CAR: f14bf5cc-9a82-4a38-bc15-d39f75ed5314
+WAS: 22052ff7-c065-42ee-bc8f-c4691c50e624
+CLE: d5a2eb42-8065-4174-ab79-0a6fa820e35e
+CIN: ad4ae08f-d808-42d5-a1e6-e9bc4e34d123
+KC: 6680d28d-d4d2-49f6-aace-5292d3ec02c2
+DEN: ce92bd47-93d5-4fe9-ada4-0fc681e6caa0
+ATL: e6aa13a4-0055-48a9-bc41-be28dc106929
+DET: c5a59daa-53a7-4de0-851f-fb12be893e9e
+NE: 97354895-8c77-4fd4-a860-32e62ea7382a
+NYJ: 5fee86ae-74ab-4bdd-8416-42a9dd9964f3
+HOU: 82d2d380-3834-4938-835f-aec541e5ece7
+CHI: 7b112545-38e6-483c-a55c-96cf6ee49cb8
+DAL: e627eec7-bbae-4fa4-8e73-8e1d6bc5c060
+BUF: 768c92aa-75ff-4a43-bcc0-f2798c2e1724
+MIA: 4809ecb0-abd3-451d-9c4a-92a90b83ca06
+SEA: 3d08af9e-c767-4f88-a7dc-b920c6d2b4a8
+SF: f0e724b0-4cbf-495a-be47-013907608da9
+NO: 0d855753-ea21-4953-89f9-0e20aff9eb73
+IND: 82cf9565-6eb9-4f01-bdbd-5aa0d472fcd9
+PHI: 386bdbf9-9eea-4869-bb9a-274b0bc66e80
+JAC: f7ddd7fa-0bae-4f90-bc8e-669e4d6cf2de
+OAK: 1c1cec48-6352-4556-b789-35304c1a6ae1
+GB: a20471b4-a8d9-40c7-95ad-90cc30e46932
+ARI: de760528-1dc0-416a-a978-b510d20692ff
+TEN: d26a1ca5-722d-4274-8f97-c92e49c96315
---
+++ app/models/team_data.rb
@@ -0,0 +1,52 @@
+def get_team_data
+    team_data ={
+        "champs" => {
+            name: "State Champs",
+            img: "https://images1.apartments.com/i2/eWDy5APj6AoNMv0_LCBpj3WC7y4OggyQ77zwcELfIHk/111/the-state-house-washington-dc-primary-photo.jpg"
+            },
+        "jake" => {
+            name: "Jake",
+            img: "http://dl9fvu4r30qs1.cloudfront.net/2d/03/66a75c2649659e104a15bac2a061/chinatown.jpg"
+            },
+        "kev" => {
+            name: "Kid Cass",
+            img: "https://www.joshuamilton.net/uploads/1/4/8/7/14875754/5981210_orig.jpg"
+            },
+        "frank" => {
+            name: "Marlon Newman",
+            img: "https://i.pinimg.com/originals/0e/77/1d/0e771d7c140e22cfdd2d276323a5f415.jpg"
+            },
+        "abs" => {
+            name: "Tom Bundchen",
+            img: "http://media.masslive.com/patriots/photo/10631045-large.jpg"
+            },
+        "elon" => {
+            name: "Evelyns",
+            img: "https://pmcvariety.files.wordpress.com/2016/01/rupaul.jpg?w=1000&h=563&crop=1"
+            },
+        "james" => {
+            name: "Little Jimmy Rags",
+            img: "http://altdaily.com/wp-content/uploads/450px-Jon-Fishman2009-12-28.jpg"
+            },
+        "gabe" => {
+            name: "Compartment Syndrome",
+            img: "http://www.thelancet.com/cms/attachment/2009245234/2032050145/gr1_lrg.jpg"
+            },
+        "noel" => {
+            name: "The First Noel",
+            img: "http://www.ajc.com/rf/image_large/Pub/p9/AJC/2018/01/09/Images/010918%20CFP%20game%20HS64.jpg"
+            },
+        "dmitri" => {
+            name: "DonAmeche",
+            img: "https://viewerscommentary.files.wordpress.com/2017/01/trading-places-ralph-bellamy-eddie-murphy-don-ameche.jpg"
+            },
+        "nathan" => {
+            name: "Steel Scorpion41",
+            img: "https://cdn.shopify.com/s/files/1/1270/1769/products/stainless-steel-scorpion-bracelet-bracelets-20715075395.jpg?v=1500781645"
+            },
+        "hugh" => {
+            name: "Pops",
+            img: "http://www.totalprosports.com/wp-content/uploads/2011/11/Screen-shot-2011-11-09-at-9.01.39-AM.png"
+            }
+    }
+end
