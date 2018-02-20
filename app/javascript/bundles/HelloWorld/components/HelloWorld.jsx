54f8dae7381e715f138c83b3ca3d5cb073b88a04
Wade Stewart <stew.wade@gmail.com>
2018-02-20 14:03:44 -0500

    Added models

--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -25,6 +25,8 @@ export default class HelloWorld extends React.Component {
     return (
       <div>
         <h3>
+          React on Rails!
+          <br/>
           Hello, {this.state.name}!
         </h3>
         <hr />
